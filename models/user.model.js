d1f5ac983c41aa62d2d941ed013c5667ae943eca
Jisun696 <istiakkashem35@gmail.com>
2025-01-17 19:15:04 +0600

    create user schema and model

---
+++ models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user", "guest"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving to database
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcryptjs.hash(this.password, 10);
+  next();
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcryptjs.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
