a35f115ca64b77c2d70575e7f43896fcf62fd6e6
Jisun696 <istiakkashem35@gmail.com>
2025-01-21 20:06:45 +0600

    install and initialize socket.io

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 // Middleware
 app.use(cors());
@@ -11,6 +20,19 @@ app.use(express.json());
 // Database connection
 import ConnectDb from "./config/ConnectDb.js";
 ConnectDb();
+
+// socket.io connection handling
+io.on("connection", (socket) => {
+  console.log("A user connected to socket.");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected from socket");
+  });
+});
+
+// httpServer.listen(3000, () => {
+//   console.log(`socket Server is running on port 3000`);
+// });
 
 // Basic route
 app.get("/", (_req, res) => {
