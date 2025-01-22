c2b4ece9dd87f0f3141a3eb682e9c555952a5b19
Jisun696 <istiakkashem35@gmail.com>
2025-01-22 16:40:42 +0600

    add isAdmin & isNotGuest middlewares and check them

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ import User from "../models/user.model.js";
 export const authenticateUser = async (req, res, next) => {
   try {
     const token = req.header("Authorization")?.replace("Bearer ", "");
-    if (!token) return res.status(401).send({ message: "unauthorized access" });
+    if (!token) return res.status(401).json({ message: "Unauthorized Access!" });
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.userId);
     if (!user) return res.status(401).json({ message: "User not found!" });
@@ -14,4 +14,13 @@ export const authenticateUser = async (req, res, next) => {
     console.log(error);
     res.status(401).json({ message: "Invalid token!" });
   }
+};
+
+export const isAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") return res.status(403).json({ message: "Unauthorized Access!" });
+  next();
+};
+export const isNotGuest = (req, res, next) => {
+  if (req.user.role === "guest") return res.status(403).json({ message: "Unauthorized Access!" });
+  next();
 };
--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { createEvent } from "../controllers/event.controller.js";
-import { authenticateUser } from "../middlewares/auth.middleware.js";
+import { authenticateUser, isNotGuest } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
-router.post("/", authenticateUser, createEvent);
+router.post("/", authenticateUser, isNotGuest, createEvent);
 
 export default router;
